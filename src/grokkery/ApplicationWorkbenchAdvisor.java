8d8296e4b0aa0848db40ca8e0e381b161a224b16
Mike Hogye <stacktracer@gmail.com>
2010-03-11 18:00:55 +0000

    Cleaned up auto-generated code.

--- src/grokkery/ApplicationWorkbenchAdvisor.java
+++ src/grokkery/ApplicationWorkbenchAdvisor.java
@@ -1,20 +1,29 @@
 package grokkery;
 
+import org.eclipse.ui.application.IWorkbenchConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 
-public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
-
-	private static final String PERSPECTIVE_ID = "grokkery.perspective";
-
-	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
-			IWorkbenchWindowConfigurer configurer) {
-		return new ApplicationWorkbenchWindowAdvisor(configurer);
-	}
-
-	public String getInitialWindowPerspectiveId() {
-		return PERSPECTIVE_ID;
-	}
-
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor
+{
+    
+    @Override
+    public void initialize(IWorkbenchConfigurer configurer)
+    {
+        configurer.setSaveAndRestore(true);
+    }
+    
+    @Override
+    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer)
+    {
+        return new ApplicationWorkbenchWindowAdvisor(configurer);
+    }
+    
+    @Override
+    public String getInitialWindowPerspectiveId()
+    {
+        return Perspective.id();
+    }
+    
 }
--- src/grokkery/Perspective.java
+++ src/grokkery/Perspective.java
@@ -3,14 +3,21 @@ package grokkery;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
-public class Perspective implements IPerspectiveFactory {
-
-	public void createInitialLayout(IPageLayout layout) {
-		String editorArea = layout.getEditorArea();
-		layout.setEditorAreaVisible(false);
-		layout.setFixed(true);
-		
-		layout.addStandaloneView("grokkery.view",  false, IPageLayout.LEFT, 1.0f, editorArea);
-	}
-
+public class Perspective implements IPerspectiveFactory
+{
+    
+    public static String id()
+    {
+        return "grokkery.perspective";
+    }
+    
+    @Override
+    public void createInitialLayout(IPageLayout layout)
+    {
+        String editorArea = layout.getEditorArea();
+        layout.setEditorAreaVisible(false);
+        
+        layout.addView(View.id(), IPageLayout.LEFT, 1.0f, editorArea);
+    }
+    
 }
