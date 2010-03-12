1708d51037e9c8c969ab4a0c24fce39953648ffd
Mike Hogye <stacktracer@gmail.com>
2010-03-12 20:04:18 +0000

    GL canvas in the Clojure view.

--- src/grokkery/View.clj
+++ src/grokkery/View.clj
@@ -1,32 +1,73 @@
 (ns grokkery.View
-
 	(:import
 		[org.eclipse.swt SWT]
-		[org.eclipse.jface.viewers TableViewer])
-
+		[javax.media.opengl GLContext]
+		[glsimple GLSimpleListener]
+		[glsimple.swt GLSimpleSwtCanvas GLSimpleSwtAnimator])
   (:gen-class
 		:extends org.eclipse.ui.part.ViewPart
 	  :state state
-	  :init init2
+	  :init init-instance
 	  :methods [#^{:static true} [id [] String]]))
 
-	(defn -init2 []
-  	[[] (ref {:viewer nil})])
 
-	(defn -getViewer [this]
-		(:viewer @(.state this)))
-
-	(defn -setViewer [this viewer]
-		(dosync (alter (.state this) assoc :viewer viewer))
-		viewer)
+(defn	-id []
+	"grokkery.view")
 
 
-	(defn	-id []
-		"grokkery.view")
+(defn -init-instance []
+ 	[[] (ref {:content-area nil, :x-axis nil, :y-axis nil})])
 
-	(defn -createPartControl [this parent]
-		(-setViewer this
-			(TableViewer. parent (reduce bit-or [SWT/MULTI SWT/H_SCROLL SWT/V_SCROLL]))))
 
-	(defn -setFocus [this]
-		(.. (-getViewer this) (getControl) (setFocus)))
+(defn get-state [this k]
+	(k @(.state this)))
+
+
+(defn draw-x-axis [gl bounds]
+	; IMPLEMENT ME
+	)
+
+
+(defn draw-y-axis [gl bounds]
+	; IMPLEMENT ME
+	)
+
+
+(defn draw-content-area [gl bounds]
+	; IMPLEMENT ME
+	)
+
+
+(defn create-canvas [parent draw]
+	(let [bounds (ref {:x 0, :y 0, :width 0, :height 0})
+				canvas (GLSimpleSwtCanvas. parent
+	  															 (into-array GLSimpleListener
+		  														   [(proxy [GLSimpleListener] []
+		  														   		
+		  								 							 		(init [context])
+		  								 							 		
+	  									 									(display [context]
+	  									 											(draw (.getGL context) @bounds))
+	  									 									
+	  									 									(reshape [context x y width height]
+	  									 										(dosync (ref-set bounds {:x x, :y y, :width width, :height height})))
+	  									 									
+	  									 									(displayChanged [context modeChanged deviceChanged]))]))]
+	  									 									
+		(.start (GLSimpleSwtAnimator. 60 canvas))
+		canvas))
+
+
+(defn -createPartControl [this parent]
+	; Use multiple assocs in same dosync
+	(dosync
+		(ref-set (.state this) {:x-axis (create-canvas parent draw-x-axis),
+	 													:y-axis (create-canvas parent draw-y-axis),
+	 													:content-area (create-canvas parent draw-content-area)})))
+
+
+(defn -setFocus [this]
+	(.setFocus (get-state this :content-area)))
+
+
+
