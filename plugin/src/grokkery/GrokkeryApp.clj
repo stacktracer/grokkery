3a5aa748be00652539ad999c3fa072631434264c
Mike Hogye <stacktracer@gmail.com>
2010-03-21 17:51:26 +0000

    Rewrote workbench window advisor in Clojure.

--- plugin/src/grokkery/GrokkeryApp.clj
+++ plugin/src/grokkery/GrokkeryApp.clj
@@ -1,20 +1,28 @@
 (ns grokkery.GrokkeryApp
   (:import
-    [grokkery ReplConsole Perspective ApplicationWorkbenchWindowAdvisor]
+    [grokkery ReplConsole Perspective]
     [org.eclipse.equinox.app IApplication IApplicationContext]
     [org.eclipse.swt.widgets Display]
+    [org.eclipse.swt.graphics Point]
     [org.eclipse.ui IWorkbench PlatformUI]
-    [org.eclipse.ui.application WorkbenchAdvisor]
+    [org.eclipse.ui.application WorkbenchAdvisor WorkbenchWindowAdvisor ActionBarAdvisor]
     [org.eclipse.ui.console ConsolePlugin])
   (:gen-class
     :implements [org.eclipse.equinox.app.IApplication]))
 
 
-(defn app-retval [workbench-retval]
-  (if
-    (= workbench-retval PlatformUI/RETURN_RESTART)
-    IApplication/EXIT_RESTART
-    IApplication/EXIT_OK))
+(defn workbench-window-advisor [configurer]
+  (proxy [WorkbenchWindowAdvisor] [configurer]
+    
+    (createActionBarAdvisor [configurer]
+      (ActionBarAdvisor. configurer))
+    
+    (preWindowOpen []
+      (doto (.getWindowConfigurer this)
+        (.setInitialSize (Point. 1024 768))
+        (.setShowCoolBar false)
+        (.setShowStatusLine false)
+        (.setTitle "Grokkery")))))
 
 
 (defn workbench-advisor []
@@ -23,8 +31,8 @@
     (initialize [configurer]
       #_(.setSaveAndRestore configurer true))
     
-    (createWorkbenchWindowAdvisor [window-configurer]
-      (ApplicationWorkbenchWindowAdvisor. window-configurer))
+    (createWorkbenchWindowAdvisor [configurer]
+      (workbench-window-advisor configurer))
     
     (getInitialWindowPerspectiveId []
       (Perspective/id))
@@ -34,6 +42,13 @@
         (doto (.. ConsolePlugin getDefault getConsoleManager)
           (.addConsoles (into-array [console]))
           (.showConsoleView console))))))
+
+
+(defn app-retval [workbench-retval]
+  (if
+    (= workbench-retval PlatformUI/RETURN_RESTART)
+    IApplication/EXIT_RESTART
+    IApplication/EXIT_OK))
 
 
 (defn -start [app context]
