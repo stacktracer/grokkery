adcca104a5ecaac1b281c8b4f1e6f0c18642c53a
Mike Hogye <stacktracer@gmail.com>
2010-03-21 15:41:00 +0000

    Rewrote ApplicationWorkbenchAdvisor in Clojure.

--- plugin/src/compile-clj.clj
+++ plugin/src/compile-clj.clj
@@ -1,8 +1,8 @@
 (binding [*compile-path* "/home/mike/projects/grokkery/code/plugin/build/classes/"]
   (dorun
   	(map compile
-  		['grokkery.GrokkeryApp
-  		 'grokkery.GrokkeryService
-  		 'grokkery.GrokkeryPlugin
-  		 'grokkery.GraphView
-       'grokkery.ReplConsole])))
+  		['grokkery.GrokkeryService
+       'grokkery.GrokkeryPlugin
+  		 'grokkery.ReplConsole
+       'grokkery.GraphView
+  		 'grokkery.GrokkeryApp])))
--- plugin/src/grokkery/GrokkeryApp.clj
+++ plugin/src/grokkery/GrokkeryApp.clj
@@ -1,9 +1,11 @@
 (ns grokkery.GrokkeryApp
   (:import
-    [grokkery ApplicationWorkbenchAdvisor]
+    [grokkery ReplConsole Perspective ApplicationWorkbenchWindowAdvisor]
     [org.eclipse.equinox.app IApplication IApplicationContext]
     [org.eclipse.swt.widgets Display]
-    [org.eclipse.ui IWorkbench PlatformUI])
+    [org.eclipse.ui IWorkbench PlatformUI]
+    [org.eclipse.ui.application WorkbenchAdvisor]
+    [org.eclipse.ui.console ConsolePlugin])
   (:gen-class
     :implements [org.eclipse.equinox.app.IApplication]))
 
@@ -13,13 +15,32 @@
     (= workbench-retval PlatformUI/RETURN_RESTART)
     IApplication/EXIT_RESTART
     IApplication/EXIT_OK))
+
+
+(defn workbench-advisor []
+  (proxy [WorkbenchAdvisor] []
     
+    (initialize [configurer]
+      #_(.setSaveAndRestore configurer true))
+    
+    (createWorkbenchWindowAdvisor [window-configurer]
+      (ApplicationWorkbenchWindowAdvisor. window-configurer))
+    
+    (getInitialWindowPerspectiveId []
+      (Perspective/id))
+    
+    (postStartup []
+      (let [console (ReplConsole.)]
+        (doto (.. ConsolePlugin getDefault getConsoleManager)
+          (.addConsoles (into-array [console]))
+          (.showConsoleView console))))))
+
 
 (defn -start [app context]
   (let [display (PlatformUI/createDisplay)]
     (try
       (app-retval
-        (PlatformUI/createAndRunWorkbench display (ApplicationWorkbenchAdvisor.)))
+        (PlatformUI/createAndRunWorkbench display (workbench-advisor)))
       (finally
         (.dispose display)))))
 
