04a6ce05d0344038f9ea41f24e9ab4864c1baf0c
Mike Hogye <stacktracer@gmail.com>
2010-03-25 02:08:09 +0000

    Repl prompt is green!

--- plugin/src/grokkery/ReplConsole.clj
+++ plugin/src/grokkery/ReplConsole.clj
@@ -22,20 +22,31 @@
   (in-ns 'user))
 
 
+(defn make-prompt-fn [out]
+  (let [writer (PrintWriter. out)]
+    #(doto writer
+      (.format "%s=> " (into-array [(ns-name *ns*)]))
+      (.flush))))
+
+
 (defn -post-init-instance [this]
+  ; Use separate stream for prompts so we can give them a different color
   (let [in (.getInputStream this)
         out (.newOutputStream this)
-        err (.newOutputStream this)]
+        err (.newOutputStream this)
+        prompt-out (.newOutputStream this)
+        prompt (make-prompt-fn prompt-out)]
     (.setColor in (Color. nil 0 0 0))
     (.setColor out (Color. nil 0 0 192))
     (.setColor err (Color. nil 255 0 0))
+    (.setColor prompt-out (Color. nil 0 160 80))
     
     (doto
       (Thread.
         #(binding [*in* (LineNumberingPushbackReader. (InputStreamReader. in))
                    *out* (PrintWriter. out true)
                    *err* (PrintWriter. err true)]
-           (clojure.main/repl :init init-repl))
+           (clojure.main/repl :init init-repl :prompt prompt))
         "Clojure Repl")
       (.setDaemon true)
       (.start))))
