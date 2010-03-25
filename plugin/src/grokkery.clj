8880973c0fbfc4fbc4368aaefe9ba15a02f73f53
Mike Hogye <stacktracer@gmail.com>
2010-03-25 22:11:51 +0000

    draw-scatter uses :pointsize, :color, and :color-fn attrs.

--- plugin/src/grokkery.clj
+++ plugin/src/grokkery.clj
@@ -6,21 +6,40 @@
     [javax.media.opengl GL]))
 
 
-(defn draw-scatter [#^GL gl data x-axfn y-axfn attrs]
+(def default-pointsize 7)
+
+
+(def default-color [0.84 0.14 0.03 1])
+
+
+(defn set-color [#^GL gl c]
+  (.glColor4f gl (c 0) (c 1) (c 2) (c 3)))
+
+
+(defn scatter-setup [#^GL gl x-axfn y-axfn attrs]
   (doto gl
     (.glEnable GL/GL_BLEND)
     (.glBlendFunc GL/GL_SRC_ALPHA GL/GL_ONE_MINUS_SRC_ALPHA)
     (.glEnable GL/GL_POINT_SMOOTH)
     (.glHint GL/GL_POINT_SMOOTH_HINT GL/GL_NICEST)
-    (.glPointSize 10)
-    (.glColor4f 0.84 0.14 0.03 1))
-  
+    (.glPointSize (or (:pointsize attrs) default-pointsize)))
+  (when-not (:color-fn attrs)
+    (set-color gl (or (:color attrs) default-color))))
+
+
+(defn scatter-point-drawfn [#^GL gl x-axfn y-axfn attrs]
+  (if-let [color-fn (:color-fn attrs)]
+    #(do
+       (set-color gl (color-fn %))
+       (.glVertex2f gl (x-axfn %) (y-axfn %)))
+    #(.glVertex2f gl (x-axfn %) (y-axfn %))))
+
+
+(defn draw-scatter [#^GL gl data x-axfn y-axfn attrs]
+  (scatter-setup gl x-axfn y-axfn attrs)
   (gl-draw gl GL/GL_POINTS
-    (try
-      (dorun
-        (map
-          #(.glVertex2f gl (x-axfn %) (y-axfn %))
-          data)))))
+    (dorun
+      (map (scatter-point-drawfn gl x-axfn y-axfn attrs) data))))
 
 
 (defn scatter-plot [fignum data]
