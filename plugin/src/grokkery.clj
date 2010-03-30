51fa442ce1c8a393ba6f001f0b5214486872bc15
Mike Hogye <stacktracer@gmail.com>
2010-03-30 22:17:54 +0000

    Renamed "scatter-plot" to just "plot".

--- plugin/src/grokkery.clj
+++ plugin/src/grokkery.clj
@@ -12,7 +12,7 @@
 (def default-point-color [0.84 0.14 0.03 1])
 
 
-(defn scatter-point-drawfn [#^GL gl x-coordfn y-coordfn attrs]
+(defn basic-point-drawfn [#^GL gl x-coordfn y-coordfn attrs]
   (if-let [colorfn (:point-colorfn attrs)]
     #(do
        (gl-set-color gl (colorfn %))
@@ -20,13 +20,13 @@
     #(.glVertex2f gl (x-coordfn %) (y-coordfn %))))
 
 
-(defn draw-scatter [#^GL gl data x-coordfn y-coordfn attrs]
+(defn draw-basic [#^GL gl data x-coordfn y-coordfn attrs]
   (.glPointSize gl (or (:point-size attrs) default-point-size))
   (gl-set-color gl (or (:point-color attrs) default-point-color))
   (gl-draw gl GL/GL_POINTS
     (dorun
-      (map (scatter-point-drawfn gl x-coordfn y-coordfn attrs) data))))
+      (map (basic-point-drawfn gl x-coordfn y-coordfn attrs) data))))
 
 
-(defn scatter-plot [fignum data]
-  (add-plot fignum data {:x first, :y second} draw-scatter {}))
+(defn plot [fignum data]
+  (add-plot fignum data {:x first, :y second} draw-basic {}))
