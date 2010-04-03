5abf4109b87f78802ccd67466903273a1109f2da
Mike Hogye <stacktracer@gmail.com>
2010-04-03 14:15:13 +0000

    Surface fiddling code is organized much better.

--- plugin/src/user.clj
+++ plugin/src/user.clj
@@ -16,15 +16,71 @@
 
 
 
+(def words-per-color 4)
+(def words-per-vert 2)
+
+
+(defn get-num-verts [nu nv]
+  (* 2 (+ nv 1) nu))
+
+
+(defn interpose-every-n [sep coll n]
+  (mapcat cons (repeat sep) (partition-all n coll)))
+
+
+(defn make-color-buffer [values value-to-color nu nv]
+  (let [num-verts (get-num-verts nu nv)
+        color-temp (float-array words-per-color)
+        buf (BufferUtil/newFloatBuffer (* words-per-color num-verts))]
+
+    (doseq [value (interpose-every-n 0 values nv)]
+      (value-to-color value color-temp)
+      (let [r (aget color-temp 0)
+            g (aget color-temp 1)
+            b (aget color-temp 2)
+            a (aget color-temp 3)]
+      (doto buf
+        (.put r) (.put g) (.put b) (.put a)
+        (.put r) (.put g) (.put b) (.put a))))
+
+    (.flip buf)
+    buf))
+
+
+(defn make-vertex-buffer [origin u v nu nv]
+  (let [num-verts (get-num-verts nu nv)
+        buf (BufferUtil/newFloatBuffer (* words-per-vert num-verts))]
+
+    (let [ni (int (dec nu)), nj (int nv)
+          ox (float (origin 0)), oy (float (origin 1))
+          ux (float (u 0)), uy (float (u 1))
+          vx (float (v 0)), vy (float (v 1))]
+
+      (loop [i (int 0)]
+        (let [xa0 (+ ox (* i ux)), ya0 (+ oy (* i uy))
+              xb0 (+ ox (* (inc i) ux)), yb0 (+ oy (* (inc i) uy))]
+          (loop [j (int 0)]
+            (let [xa (+ xa0 (* j vx)), ya (+ ya0 (* j vy))
+                  xb (+ xb0 (* j vx)), yb (+ yb0 (* j vy))]
+              (doto buf
+                (.put xa) (.put ya)
+                (.put xb) (.put yb)))
+            (when (< j nj) (recur (inc j)))))
+        (when (< i ni) (recur (inc i)))))
+
+    (.flip buf)
+    buf))
 
 
 
+(def nu 113)
+(def nv 47)
+(def values (take (* nu nv) (repeatedly rand)))
+(def origin [0 0])
+(def u [0.9 -0.1])
+(def v [0.4 2.3])
 
-
-
-
-
-(defn cell-colorfn [cell-value #^floats rgba-array]
+(defn value-to-color [cell-value #^floats rgba-array]
   (let [one (float 1)
         zero (float 0)
         v (float cell-value)]
@@ -35,92 +91,13 @@
       (aset 3 one))))
 
 
-(defn interpose-every-n [sep coll n]
-  (mapcat cons (repeat sep) (partition-all n coll)))
-
-
-(defn duplicate [coll]
-  (interleave coll coll))
-
-
-
-(defn +sv [sa vb]
-  [(+ sa (first vb)) (+ sa (second vb))])
-
-
-(defn *sv [sa vb]
-  [(* sa (first vb)) (* sa (second vb))])
-
-
-(defn +vs [va sb]
-  [(+ (first va) sb) (+ (second va) sb)])
-
-
-(defn *vs [va sb]
-  [(* (first va) sb) (* (second va) sb)])
-
-
-(defn +vv [va vb]
-  [(+ (first va) (first vb)) (+ (second va) (second vb))])
-
-
-(defn *vv [va vb]
-  [(* (first va) (first vb)) (* (second va) (second vb))])
-
-
-
-(def n1 113)
-(def n2 47)
-(def values (take (* n1 n2) (repeatedly rand)))
-(def origin [0 0])
-(def v1 [0.9 -0.1])
-(def v2 [0.4 2.3])
-
 (defn draw-surf [#^GL gl data x-coordfn y-coordfn attrs]
+  (let [num-verts (get-num-verts nu nv)
+        verts (make-vertex-buffer origin u v nu nv)
+        colors (make-color-buffer values value-to-color nu nv)]
 
-  (.glShadeModel gl GL/GL_FLAT)
-  (.glEnable gl GL/GL_CULL_FACE)
-
-
-  (let [words-per-vert 2
-        words-per-color 4
-        color-temp (float-array words-per-color)
-        num-verts (* 2 (+ n2 1) n1)
-        verts (BufferUtil/newFloatBuffer (* words-per-vert num-verts))
-        colors (BufferUtil/newFloatBuffer (* words-per-color num-verts))]
-
-    (doseq [value (interpose-every-n 0 values n2)]
-      (cell-colorfn value color-temp)
-      (let [r (aget color-temp 0)
-            g (aget color-temp 1)
-            b (aget color-temp 2)
-            a (aget color-temp 3)]
-      (doto colors
-        (.put r) (.put g) (.put b) (.put a)
-        (.put r) (.put g) (.put b) (.put a))))
-
-    (.flip colors)
-
-
-    (let [ni1 (int (dec n1)), ni2 (int n2)
-          x-orig (float (origin 0)), y-orig (float (origin 1))
-          x-v1 (float (v1 0)), y-v1 (float (v1 1))
-          x-v2 (float (v2 0)), y-v2 (float (v2 1))]
-
-      (loop [i1 (int 0)]
-        (let [xa-col (+ x-orig (* i1 x-v1)), ya-col (+ y-orig (* i1 y-v1))
-              xb-col (+ x-orig (* (inc i1) x-v1)), yb-col (+ y-orig (* (inc i1) y-v1))]
-          (loop [i2 (int 0)]
-            (let [xa (+ xa-col (* i2 x-v2)), ya (+ ya-col (* i2 y-v2))
-                  xb (+ xb-col (* i2 x-v2)), yb (+ yb-col (* i2 y-v2))]
-              (doto verts
-                (.put xa) (.put ya)
-                (.put xb) (.put yb)))
-            (when (< i2 ni2) (recur (inc i2)))))
-        (when (< i1 ni1) (recur (inc i1)))))
-
-    (.flip verts)
-
+    (.glShadeModel gl GL/GL_FLAT)
+    (.glEnable gl GL/GL_CULL_FACE)
 
     (.glEnableClientState gl GL/GL_VERTEX_ARRAY)
     (.glEnableClientState gl GL/GL_COLOR_ARRAY)
